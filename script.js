f34d76e12da8fc5bb19f25e5d0a32421b3ff5017
DEEPANSHU <deepanshufea07@gmail.com>
2024-12-23 13:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-//complete this code
-class Rectangle {}
- constructor(width, height) {
-	 this._width = width;
-	    this._height = height;}
- get width() {
+// Define the Rectangle class
+class Rectangle {
+  // Constructor accepts width and height as arguments
+  constructor(width, height) {
+    this._width = width;  // Private property for width
+    this._height = height; // Private property for height
+  }
+
+  // Getter method for width
+  get width() {
     return this._width;
   }
-get height(){
-	return this._height;
-	
-}
- getArea() {
+
+  // Getter method for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate the area of the rectangle
+  getArea() {
     return this._width * this._height;
   }
 }
-class Square extends Animal {
-	constructor(side) { super(side, side);
+
+// Define the Square class that extends Rectangle
+class Square extends Rectangle {
+  // Constructor accepts side as the argument
+  constructor(side) {
+    // Call the parent class constructor with side for both width and height
+    super(side, side);
   }
-}
- getPerimeter() {
-    return 4 * this.width; 
+
+  // Method to calculate the perimeter of the square
+  getPerimeter() {
+    return 4 * this.width; // Since width = height for square, we can use width
   }
 }
 
